87f09dc3e2ffd19f8599f159cec591856ccbc8f4
s <s@k.com>
2017-09-27 21:03:04 -0600

    Added new HelloWorld again line.

--- CoreAppGitTInker/CoreAppGitTInker/Program.cs
+++ CoreAppGitTInker/CoreAppGitTInker/Program.cs
@@ -7,6 +7,7 @@ namespace CoreAppGitTInker
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Again!");
         }
     }
 }
